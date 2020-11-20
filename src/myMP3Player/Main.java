707ad1e6f6e22f961346ba3397db8e68794f92ee
kevingrillet <kevin.grillet@gmail.com>
2020-11-20 09:57:36 +0100

    Add fxml

--- src/myMP3Player/Main.java
+++ src/myMP3Player/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MyMP3Player.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root/*, 300, 275*/));
         primaryStage.show();
     }
 
