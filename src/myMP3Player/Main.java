99f770222f43fa38275239a572b8f64d4890e23b
david <david.rochinhachaves@epfl.ch>
2020-11-21 14:06:47 +0100

    Merge branch 'main' of C:\Users\chave\IdeaProjects\MyMP3Player with conflicts.

--- src/myMP3Player/Main.java
+++ src/myMP3Player/Main.java
@@ -4,25 +4,49 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 /**
- * Main of MyMP3Player
+ *  Main of MyMP3Player
  */
 public class Main extends Application {
 
-    /**
-     * @param args launch arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MyMP3Player.fxml"));
         primaryStage.setTitle("MyMP3Player");
         primaryStage.setScene(new Scene(root/*, 300, 275*/));
         primaryStage.show();
+
+
+
+        /*MusicPlayer musicPlayer = new MusicPlayer();
+        FileChooser fileChooser = new FileChooser();
+        List<File> fileList = fileChooser.showOpenMultipleDialog(null);
+        musicPlayer.addMusic(fileList.stream().map(e -> e.getPath()).collect(Collectors.toList()));
+
+        musicPlayer.play();
+        TimeUnit.SECONDS.sleep(2);
+        musicPlayer.setVolume(1);
+        TimeUnit.SECONDS.sleep(2);
+        musicPlayer.setVolume(0.1f);
+        TimeUnit.SECONDS.sleep(2);
+        musicPlayer.setVolume(0.5f);*/
+
+
+    }
+
+    /**
+     *
+     * @param args launch arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
 }
