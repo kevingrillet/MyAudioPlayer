3d61fdc059564c06d3187c29a6a61443ad1dc9fe
david <david.rochinhachaves@epfl.ch>
2020-11-21 15:25:20 +0100

    modification on music player

--- src/myMP3Player/MusicPlayer.java
+++ src/myMP3Player/MusicPlayer.java
@@ -1,5 +1,7 @@
 package myMP3Player;
 
+import javafx.scene.media.Media;
+
 import javax.sound.sampled.*;
 import java.io.File;
 import java.util.Collection;
@@ -9,7 +11,7 @@ import java.util.Queue;
 /**
  * My Music Player
  */
-public class MusicPlayer {
+public class MusicPlayer implements MyAudioPlayer{
 
     private Clip clip;
     private final Queue<String> musics;
@@ -22,30 +24,32 @@ public class MusicPlayer {
      * Default constructor
      * @param mixerInfo (Mixer.Info) : Mixer info of the output port wanted
      */
-    public MusicPlayer(Mixer.Info mixerInfo){
+    public MusicPlayer(Mixer.Info mixerInfo) throws LineUnavailableException {
         musics = new LinkedList<>();
         currentMusic = null;
         status = Status.NOTSTART;
         duration = 0l;
-        try {
-            clip = AudioSystem.getClip(mixerInfo);
-        } catch (Exception e){
-            clip = null;
-        }
+        clip = AudioSystem.getClip(mixerInfo);
     }
 
     /**
      * Constructor with default mixer
      */
-    public MusicPlayer(){
+    public MusicPlayer() throws LineUnavailableException {
         this(AudioSystem.getMixer(null).getMixerInfo());
+    }
+
+    @Override
+    public void add(String path) {
+        musics.add(path);
     }
 
     /**
      * Add music to the listening queue
      * @param musics (Collection<String>) : Collection of paths to the musics
      */
-    public void addMusic(Collection<String> musics){
+    @Override
+    public void addAll(Collection<String> musics){
         this.musics.addAll(musics);
     }
 
@@ -54,6 +58,7 @@ public class MusicPlayer {
      */
     public void changeOutput(Mixer.Info mixerInfo){
         pause();
+        Clip tmp = clip;
         try {
             clip = AudioSystem.getClip(mixerInfo);
             if (status != Status.NOTSTART) {
@@ -61,18 +66,27 @@ public class MusicPlayer {
             }
         } catch (Exception e){
             System.out.println(e.getMessage());
+            clip = tmp;
         }
         resume();
+    }
+
+    public String getName(){
+        Media media = new Media(currentMusic.toURI().toString());
+        String name = currentMusic.getName().substring(0, currentMusic.getName().length() - 4);
+        Object o = media.getMetadata().getOrDefault("title", null);
+        return o == null ? name : o.toString();
     }
     /**
      * Play the music, resume if paused
      */
+    @Override
     public void play(){
         if (!musics.isEmpty() && currentMusic == null) {
             try {
                 currentMusic = new File(musics.poll());
                 clip.open(AudioSystem.getAudioInputStream(currentMusic));
-                duration = getDuration(currentMusic);
+                duration = getDurationFile(currentMusic);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
@@ -87,9 +101,23 @@ public class MusicPlayer {
         }
     }
 
+    @Override
+    public void previous() {
+        seek(0);
+    }
+
+    @Override
+    public void remove(int index) {
+        if (!musics.isEmpty()) {
+            ((LinkedList) musics).remove(index);
+        }
+    }
+
+
     /**
      * Stop music
      */
+    @Override
     public void stop(){
         if(currentMusic != null){
             clip.stop();
@@ -101,6 +129,7 @@ public class MusicPlayer {
     /**
      * Pause music
      */
+    @Override
     public void pause(){
         if (currentMusic != null && status == Status.PLAYING){
             time = clip.getMicrosecondPosition();
@@ -123,6 +152,7 @@ public class MusicPlayer {
     /**
      * Play the next music
      */
+    @Override
     public void next(){
         if (musics.isEmpty()){
             stop();
@@ -137,7 +167,7 @@ public class MusicPlayer {
                 System.out.println(e.getMessage());
                 clip.stop();
             }
-            duration = getDuration(currentMusic);
+            duration = getDurationFile(currentMusic);
             stop();
             play();
         }
@@ -147,7 +177,7 @@ public class MusicPlayer {
      * Get the current time
      * @return (long): current time in milliseconds
      */
-    public long getTime(){
+    public double getTime(){
         return clip.getMicrosecondPosition();
     }
 
@@ -155,12 +185,13 @@ public class MusicPlayer {
      * Get End time
      * @return (long): end time in milliseconds
      */
-    public long getEndTime() {
+    public double getDuration() {
         return duration;
     }
 
     /**
-     *Tells if a music has ended
+     * Tells if a music has ended
+     * @return (if the current music has ended)
      */
     public boolean hasEnded() {
         return clip.getMicrosecondPosition() >= duration;
@@ -168,6 +199,7 @@ public class MusicPlayer {
 
     /**
      * Return current status
+     * @return (MusicPlayer.Status) : current status
      */
     public Status getStatus() {
         return status;
@@ -177,22 +209,31 @@ public class MusicPlayer {
      * Go to time position
      * @param time (long): time in milliseconds
      */
-    public void seek(long time){
-        clip.setMicrosecondPosition(time);
+    public void seek(double time){
+        clip.setMicrosecondPosition((long) time);
     }
 
     /**
      * Value between 0 and 1
+     * @param v (double): volume between 0 and 1
      */
-    public void setVolume(float v){
+    public void setVolume(double v){
         assert (v >= 0 && v <= 1);
         FloatControl gainControl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
-        gainControl.setValue(20f * (float) Math.log10(v));
+        gainControl.setValue((20f *  (float) Math.log10(v)));
+    }
+
+    public double getVolume(){
+        if (clip != null) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.VOLUME);
+            return gainControl.getValue();
+        }else {
+            return -1f;
+        }
     }
 
 
-
-    private static long getDuration(File file) {
+    private static long getDurationFile(File file) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
             AudioFormat format = audioInputStream.getFormat();
