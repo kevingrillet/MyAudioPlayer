647eef47d8f042887f1bc61ca21f3aac5c2a59bf
kevingrillet <kevin.grillet@gmail.com>
2020-11-21 09:09:55 +0100

    Add class Tools.java

--- src/myMP3Player/Controller.java
+++ src/myMP3Player/Controller.java
@@ -318,57 +318,12 @@ public class Controller {
      */
     private void updateTimeValue(){
         Duration currentTime = mediaPlayer.getCurrentTime();
-        labelPlayerTime.setText(formatTime(currentTime, duration));
+        labelPlayerTime.setText(Tools.formatTime(currentTime, duration));
         sliderPlayerTime.setDisable(duration.isUnknown());
         if (!sliderPlayerTime.isDisabled()
                 && duration.greaterThan(Duration.ZERO)
                 && !sliderPlayerTime.isValueChanging()) {
             sliderPlayerTime.setValue(currentTime.divide(duration.toMillis()).toMillis() * 100.0);
-        }
-    }
-
-    /**
-     *
-     * @param elapsed Current time of the Media
-     * @param duration Duration of the Media
-     * @return String 00:00/00:00
-     */
-    private static String formatTime(Duration elapsed, Duration duration) {
-        int intElapsed = (int)Math.floor(elapsed.toSeconds());
-        int elapsedHours = intElapsed / (60 * 60);
-        if (elapsedHours > 0) {
-            intElapsed -= elapsedHours * 60 * 60;
-        }
-        int elapsedMinutes = intElapsed / 60;
-        int elapsedSeconds = intElapsed - elapsedHours * 60 * 60
-                - elapsedMinutes * 60;
-
-        if (duration.greaterThan(Duration.ZERO)) {
-            int intDuration = (int)Math.floor(duration.toSeconds());
-            int durationHours = intDuration / (60 * 60);
-            if (durationHours > 0) {
-                intDuration -= durationHours * 60 * 60;
-            }
-            int durationMinutes = intDuration / 60;
-            int durationSeconds = intDuration - durationHours * 60 * 60 -
-                    durationMinutes * 60;
-            if (durationHours > 0) {
-                return String.format("%d:%02d:%02d/%d:%02d:%02d",
-                        elapsedHours, elapsedMinutes, elapsedSeconds,
-                        durationHours, durationMinutes, durationSeconds);
-            } else {
-                return String.format("%02d:%02d/%02d:%02d",
-                        elapsedMinutes, elapsedSeconds,durationMinutes,
-                        durationSeconds);
-            }
-        } else {
-            if (elapsedHours > 0) {
-                return String.format("%d:%02d:%02d", elapsedHours,
-                        elapsedMinutes, elapsedSeconds);
-            } else {
-                return String.format("%02d:%02d",elapsedMinutes,
-                        elapsedSeconds);
-            }
         }
     }
 }
---
+++ src/myMP3Player/Tools.java
@@ -0,0 +1,53 @@
+package myMP3Player;
+
+import javafx.util.Duration;
+
+/**
+ *  Tools for easier life
+ */
+public class Tools {
+    /**
+     *
+     * @param elapsed Current time of the Media
+     * @param duration Duration of the Media
+     * @return String 00:00/00:00
+     */
+    public static String formatTime(Duration elapsed, Duration duration) {
+        int intElapsed = (int)Math.floor(elapsed.toSeconds());
+        int elapsedHours = intElapsed / (60 * 60);
+        if (elapsedHours > 0) {
+            intElapsed -= elapsedHours * 60 * 60;
+        }
+        int elapsedMinutes = intElapsed / 60;
+        int elapsedSeconds = intElapsed - elapsedHours * 60 * 60
+                - elapsedMinutes * 60;
+
+        if (duration.greaterThan(Duration.ZERO)) {
+            int intDuration = (int)Math.floor(duration.toSeconds());
+            int durationHours = intDuration / (60 * 60);
+            if (durationHours > 0) {
+                intDuration -= durationHours * 60 * 60;
+            }
+            int durationMinutes = intDuration / 60;
+            int durationSeconds = intDuration - durationHours * 60 * 60 -
+                    durationMinutes * 60;
+            if (durationHours > 0) {
+                return String.format("%d:%02d:%02d/%d:%02d:%02d",
+                        elapsedHours, elapsedMinutes, elapsedSeconds,
+                        durationHours, durationMinutes, durationSeconds);
+            } else {
+                return String.format("%02d:%02d/%02d:%02d",
+                        elapsedMinutes, elapsedSeconds,durationMinutes,
+                        durationSeconds);
+            }
+        } else {
+            if (elapsedHours > 0) {
+                return String.format("%d:%02d:%02d", elapsedHours,
+                        elapsedMinutes, elapsedSeconds);
+            } else {
+                return String.format("%02d:%02d",elapsedMinutes,
+                        elapsedSeconds);
+            }
+        }
+    }
+}
